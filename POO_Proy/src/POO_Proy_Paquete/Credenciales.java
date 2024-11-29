1d53cb88e8db3cf8418b80c7b98de450f1994e11
Angel-dt1511 <angel.vdx76@gmail.com>
2024-11-28 21:57:55 -0500

    nombres

--- POO_Proy/src/POO_Proy_Paquete/Credenciales.java
+++ POO_Proy/src/POO_Proy_Paquete/Credenciales.java
@@ -19,20 +19,20 @@ interface Credenciales {
     String idE5 = "E5";
     String passE5 = "pass5";    
     //nombres y apellidos de docentes
-    String nD1 = "Nombre_Docente1";
-    String appD1 = "Apellido_Docente1";
-    String nD2 = "Nombre_Docente2";
-    String appD2 = "Apellido_Docente2";
+    String nD1 = "Elvis";
+    String appD1 = "ASTUHUAMAN";
+    String nD2 = "Jose";
+    String appD2 = "Bustamante";
     
     //nombres y apellidos de estudiantes
-    String nE1 = "Nombre_Estudianet1";
-    String appE1 = "Apellido_Estudiante1";
-    String nE2 = "Nombre_Estudiante2";
-    String appE2 = "Apellido_Estudiante2";
-    String nE3 = "Nombre_Estudiante3";
-    String appE3 = "Apellido_Estudiante3";
-    String nE4 = "Nombre_Estudianete4";
-    String appE4 = "Apellido_Estudiante4";
-    String nE5 = "Nombre_Estudiante5";
-    String appE5 = "Apellido_Estudiante5";
+    String nE1 = "Fabian";
+    String appE1 = "MArroquin";
+    String nE2 = "Jose";
+    String appE2 = "Carhuacusma";
+    String nE3 = "Angel";
+    String appE3 = "Oregon";
+    String nE4 = "Jeffrey";
+    String appE4 = "Balbuena";
+    String nE5 = "Marvin";
+    String appE5 = "Huanca";
 }
